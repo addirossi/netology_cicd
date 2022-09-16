e6915fc9a2d77074179475512b88a32b07d40565
addirossi <addirossi@gmail.com>
2022-09-16 09:18:03 +0600

    Add project

---
+++ main/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestSomething(TestCase):
+    def test_dummy(self):
+        self.assertEqual(5*10, 50)
+
+    def test_dummy_2(self):
+        self.assertIn(5, [1, 2, 3, 4, 5])
